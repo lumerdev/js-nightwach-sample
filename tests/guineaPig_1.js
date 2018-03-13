9d10ee4dea93a40fde7322f1315a8713c87ec32b
Van H <vhuynh@lumeris.com>
2018-03-13 08:45:56 -0700

    Initial commit

---
+++ tests/guineaPig_1.js
@@ -0,0 +1,33 @@
+var https = require('https');
+var SauceLabs = require("saucelabs");
+
+module.exports = {
+
+    '@tags': ['guineaPig'],
+
+    'Fail 2': function(client) {
+        client
+            .url('https://saucelabs-sample-test-frameworks.github.io/training-test-page')
+            .waitForElementVisible('body', 1000)
+            .assert.title('Banana');
+    },
+
+    'Not Fail 1': function(client) {
+        client
+            .url('https://saucelabs-sample-test-frameworks.github.io/training-test-page')
+            .waitForElementVisible('body', 1000)
+            .assert.title('I am a page title - Sauce Labs');
+    },
+
+
+
+    afterEach: function(client, done) {
+        client.customSauceEnd();
+
+        setTimeout(function() {
+            done();
+        }, 1000);
+
+    }
+
+};
